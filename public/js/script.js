a5e8c0bfc97ab46d6295280ced67a97a501abecc
imjord <dean.jay.goat@gmail.com>
2021-08-10 14:33:28 -0600

    working css and javascript

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+const JSel = document.getElementById("javascript-button")
+
+
+
+
+
+
+JSel.addEventListener("click", ()=> {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+
+})
+
+
+
